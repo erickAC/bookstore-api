4a10566b40f90c311db653c55826b004b876942b
erickAC <cerickandrade@gmail.com>
2022-02-24 12:47:23 -0300

    delete para categoria

--- src/main/java/com/api/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/api/bookstore/resources/CategoriaResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,34 +26,39 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService categoriaService;
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
 		Categoria obj = categoriaService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = categoriaService.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
 		obj = categoriaService.create(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
-	
+
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO obj) {
 		Categoria newObj = categoriaService.update(id, obj);
 		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
 	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		categoriaService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
 
 //localhost:8080/categorias/1
-	
