20b99f4fee8912e7c1a3093bde74ff0a11047b6d
erickAC <cerickandrade@gmail.com>
2022-02-24 08:28:08 -0300

    post para categoria

--- src/main/java/com/api/bookstore/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/api/bookstore/services/exceptions/ObjectNotFoundException.java
@@ -5,14 +5,10 @@ public class ObjectNotFoundException extends RuntimeException{
 
 	public ObjectNotFoundException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ObjectNotFoundException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
-
-	
 	
 }
