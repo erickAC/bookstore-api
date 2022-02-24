eb110116cd82e2ca4e9513e753a2b48c204ec499
erickAC <cerickandrade@gmail.com>
2022-02-24 11:20:28 -0300

    update para categoria

--- src/main/java/com/api/bookstore/services/CategoriaService.java
+++ src/main/java/com/api/bookstore/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.api.bookstore.domain.Categoria;
+import com.api.bookstore.dtos.CategoriaDTO;
 import com.api.bookstore.repositories.CategoriaRepository;
 import com.api.bookstore.services.exceptions.ObjectNotFoundException;
 
@@ -28,6 +29,13 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return categoriaRepository.save(obj);
 	}
 	
